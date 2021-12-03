ea6d49aecbddda18e123cf3a8e258bfd78c2e09a
Víctor Auñón <hola@victoraunon.com>
2021-12-03 11:20:52 +0100

    Change phone number

--- scripts/contactData.js
+++ scripts/contactData.js
@@ -1,6 +1,6 @@
 const data = {
     name: "Mi nombre",
-    phone: "34653058933",
+    phone: "34666666666",
     daysOpen: [1, 2, 3, 4, 5],
     // Each hour includes from 0 to 59 minutes
     timeOpenUTC: [7, 8, 9, 10, 11, 12, 14, 15, 16],
